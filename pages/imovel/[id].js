1fcc22a6aa9f7dc21d86834f1e7419670464f547
Jorge Serras <caveiroserras@gmail.com>
2020-10-19 18:50:13 +0100

    Fixes UI

--- pages/imovel/[id].js
+++ pages/imovel/[id].js
@@ -378,12 +378,11 @@ const Imovel = ({ params, signedIn }) => {
     const ImoStatusCode = statusImo || 'Not published'
     const isImoPending = ImoStatusCode.includes('pending') || ImoStatusCode.includes('pendente') ? true : false
 
-    const objectiveStatus = data && data['imovel-estado'].length ? data['imovel-estado'][0][0] === '77' ? 'A arrendar' : data['imovel-estado'][0] === '78' ? 'A vender' : data['imovel-estado'][0] === '174' ? 'Arrendado' : data['imovel-estado'][0] === '175' ? 'Vendido' : null : null
-    const type = data && data['imovel-tipo'].length ? data['imovel-tipo'][0] === 34 ? 'Moradia' : 'Apartamento' : null
+    const objectiveStatus = data && data['imovel-estado'] && data['imovel-estado'].length ? data['imovel-estado'][0] === 77 ? 'A arrendar' : data['imovel-estado'][0] === 78 ? 'A vender' : data['imovel-estado'][0] === 174 ? 'Arrendado' : data['imovel-estado'][0] === 175 ? 'Vendido' : null : null
+    const type = data && data['imovel-tipo'] && data['imovel-tipo'].length ? data['imovel-tipo'][0] === 34 ? 'Moradia' : 'Apartamento' : null
 
     if (loading)
         return <Loading message={loading} />;
-
 
     return (
         <Layout
