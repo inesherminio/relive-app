8e39fed808d42bbb16d262b44367d4a2272659d8
Jorge Serras <caveiroserras@gmail.com>
2020-10-19 17:07:23 +0100

    More info on Property page

--- pages/imovel/[id].js
+++ pages/imovel/[id].js
@@ -398,19 +398,19 @@ const Imovel = ({ params, signedIn }) => {
                 {data.title ?
                     <>
                         <h2>{data.title.rendered}</h2>
-                        <Grid container justify="center">
-                            <Grid item>
+                        <Grid container justify="flex-start">
+                            <Grid item xs={3}>
                                 <h3>Estado Website: <span style={{ color: displayStatus === 'Publico' ? '#82ca9d' : 'red' }}>{displayStatus}</span></h3>
                             </Grid>
-                            <Grid item>
+                            <Grid item xs={3}>
                                 <h3><span style={{ color: '#82ca9d' }}>{type}</span></h3>
                             </Grid>
-                            <Grid item>
+                            <Grid item xs={3}>
                                 <h3><span style={{ color: '#82ca9d' }}>{objectiveStatus}</span></h3>
                             </Grid>
                         </Grid>
-                        <Grid container justify="center">
-                            <Grid item>
+                        <Grid container justify="flex-start">
+                            <Grid item xs={3}>
                                 <h3>Estado Imovirtual: <span style={{ color: ImoStatusCode === 'active' ? '#82ca9d' : 'red' }}>{ImoStatusCode}</span></h3>
                             </Grid>
                         </Grid>
